6ca33c985dc70defe3735babd220ec0152d11ffe
Isaias silva <Isaiasprog@outlook.com>
2024-10-18 16:06:15 -0300

    Foi atualizado o site para versão 1.0.1 seguindo as instruções do cliente.

--- scripts.js
+++ scripts.js
@@ -28,3 +28,31 @@ window.addEventListener("scroll", revealOnScroll);
 document.addEventListener("DOMContentLoaded", () => {
     revealOnScroll();
 });
+
+// Adiciona JavaScript para a funcionalidade da imagem
+const feedbackImages = document.querySelectorAll(".feedback-img");
+
+feedbackImages.forEach(image => {
+    image.addEventListener("click", () => {
+        const fullSrc = image.dataset.fullSrc;
+        const modal = document.createElement("div");
+        modal.classList.add("modal");
+        modal.innerHTML = `
+            <img src="${fullSrc}" alt="Imagem completa" class="modal-img">
+            <span class="close-modal">×</span>
+        `;
+        document.body.appendChild(modal);
+
+        const closeModalButton = modal.querySelector(".close-modal");
+        closeModalButton.addEventListener("click", () => {
+            document.body.removeChild(modal);
+        });
+
+        modal.style.display = "block"; // Mostra o modal
+
+        // Adiciona a transição ao modal
+        setTimeout(() => {
+            modal.style.opacity = "1";
+        }, 10);
+    });
+});
